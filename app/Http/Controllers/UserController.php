0292ffbb3bf42f68668cdd571a0f98cdbca7aa4b
sachin giri <girikomal3327@gmail.com>
2025-08-09 09:03:32 +0545

    Initial commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function register(Request $request)
+    {
+        // Validate the request data
+        $incomingFields = $request->validate([
+            'name' => 'required|string|max:255|unique:users,name',
+            'email' => 'required|string|email|max:255|unique:users,email',
+            'password' => 'required|string|min:8',
+        ]);
+
+
+        $incomingFields['password'] = bcrypt($incomingFields['password']);
+        $user = User::create($incomingFields);
+        auth()->login($user);
+       return redirect('/');
+    }
+
+    public function logout()
+    {
+        auth()->logout();
+        return redirect('/');
+    }
+
+    public function login(Request $request)
+    {
+        $incomingFields = $request->validate([
+            'loginname' => 'required|string',
+            'loginpassword' => 'required|string',
+        ]);
+        if (auth()->attempt(['name' => $incomingFields['loginname'], 'password' => $incomingFields['loginpassword']])){
+            $request->session()->regenerate();
+        }
+        return redirect('/');
+    }
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    @auth
+    <p>congrats you are logged in!</p>
+    <form action="/logout" method="POST">
+        @csrf
+        <button type="submit">Logout</button>
+    </form>
+    @else
+    <div style="border: 3px solid black;">
+        <h2>Register</h2>
+        <form action="/register" method="POST">
+            @csrf
+            <label for="name">Name:</label>
+            <input type="text" id="name" name="name" required>
+            <br>
+            <label for="email">Email:</label>
+            <input type="email" id="email" name="email" required>
+            <br>
+            <label for="password">Password:</label>
+            <input type="password" id="password" name="password" required>
+            <br>
+            <button type="submit">Register</button>
+        </form>
+    </div>
+    <div style="border: 3px solid black;">
+        <h2>Login</h2>
+        <form action="/login" method="POST">
+            @csrf
+            <label for="loginname">Name:</label>
+            <input type="text" id="loginname" name="loginname" required>
+            <br>
+            <label for="loginpassword">Password:</label>
+            <input type="password" id="loginpassword" name="loginpassword" required>
+            <br>
+            <button>Log in</button>
+        </form>
+    </div>
+    @endauth
+   
+</body>
+</html>
